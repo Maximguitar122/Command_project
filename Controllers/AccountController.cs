052cb327a91f5002e922acd5c55450d5ab389b24
Maxym <Maxym@DESKTOP-HS5N4TR>
2026-04-04 22:30:49 +0300

    commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Luftreise_Command_project_.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();  
+        }
+
+        public IActionResult Sing_UP()
+        {
+            return View();
+        }
+    }
+}
--- Models/SearchModels.cs
+++ Models/SearchModels.cs
@@ -5,5 +5,6 @@
         public string From { get; set; } = string.Empty;
         public string To { get; set; } = string.Empty;
         public DateTime FlightDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
     }
 }
