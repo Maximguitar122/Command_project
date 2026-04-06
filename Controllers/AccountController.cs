5bcaf6f2db81a35511e3f817833e618962703e50
Maxym <Maxym@DESKTOP-HS5N4TR>
2026-04-06 19:33:45 +0300

    Frontend

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -8,8 +8,40 @@ namespace Luftreise_Command_project_.Controllers
         {
             return View();  
         }
+        [HttpGet]
+        public IActionResult Sign_Up()
+        {
+            return View();
+        }
 
-        public IActionResult Sing_UP()
+        [HttpPost]
+        public IActionResult Sign_UP(
+            string FullName,
+             string Email,
+             string Phone,
+             string City,
+             string Country,
+             DateTime? BirthDate,
+             string Password,
+             string ConfirmPassword)
+        {
+            TempData["SuccessMessage"] = "Ви успішно зареєструвались!";
+            return RedirectToAction("Login");
+        }
+
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ForgotPassword(string Email)
+        {
+            TempData["SuccessMessage"] = "Інструкції надіслані на email!";
+            return RedirectToAction("Login");
+        }
+
+        public IActionResult Profile()
         {
             return View();
         }
---
+++ Models/LoginModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Luftreise_Command_project_.Models
+{
+    public class LoginModel
+    {
+
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
